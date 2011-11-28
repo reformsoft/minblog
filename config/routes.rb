3030db66ee550dc1a63b893f7a5131bca5ca139d
deedoubleyou <deedoubleyou@gmail.com>
2011-11-28 20:55:48 +0000

    * Made home#index the default route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Minblog::Application.routes.draw do
 
   resources :posts
 
+  root :to => 'home#index', :as => 'home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
